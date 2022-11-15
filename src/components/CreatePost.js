c6446111b1be13fc0fd92d3d7c010c9d3640ea8a
Kazuki624 <audi_kazuki0624@yahoo.co.jp>
2022-11-15 22:05:34 +0900

    making and styling create post page

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import "./CreatePost.css";
 
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
+    <div className='createPostPage'>
+        <div className='postContainer'>
+            <h1>記事を投稿する</h1>
+            <div className='inputPost'>
+                <div>タイトル</div>
+                <input type="text" placeholder="タイトルを入力してください"></input>
+            </div>
+            <div className='inputPost'>
+                <div>投稿内容</div>
+                <textarea type="text" placeholder="投稿内容を入力してください"></textarea>
+            </div>
+            <button className='postButton'>投稿する</button>
+        </div>
+    </div>
   )
 }
 
