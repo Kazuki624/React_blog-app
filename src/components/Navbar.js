f59eb4213059b2b7bac913892bae6f9437856ec9
Kazuki624 <audi_kazuki0624@yahoo.co.jp>
2022-11-15 17:32:56 +0900

    styling and install fontawesome

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouseUser } from '@fortawesome/free-solid-svg-icons';
+import { faPenNib } from '@fortawesome/free-solid-svg-icons';
+import { faRightToBracket } from '@fortawesome/free-solid-svg-icons';
+
 
 const Navbar = () => {
   return (
     <nav>
-        <Link to='/'>ホーム</Link>
-        <Link to='/createpost'>記事投稿</Link>
-        <Link to='/login'>ログイン</Link>
+        <Link to='/'><FontAwesomeIcon icon={faHouseUser} />ホーム</Link>
+        <Link to='/createpost'><FontAwesomeIcon icon={faPenNib} />記事投稿</Link>
+        <Link to='/login'><FontAwesomeIcon icon={faRightToBracket} />ログイン</Link>
     </nav>
   )
 }
