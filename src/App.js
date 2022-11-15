e58fd3d5dec27a4c6fc6da60f4daaa553f545baa
Kazuki624 <audi_kazuki0624@yahoo.co.jp>
2022-11-15 21:21:23 +0900

    Save authentication state to local storage

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import CreatePost from './components/CreatePost';
 import Login from './components/Login';
 import Logout from './components/Logout';
 import Navbar from './components/Navbar';
+import { useState } from 'react';
 
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false) //ログインしているかしていないかを管理
+
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route  path='/' element={<Home />} />
         <Route  path='/createpost' element={<CreatePost />} />
-        <Route  path='/login' element={<Login />} />
+        <Route  path='/login' element={<Login setIsAuth={setIsAuth}/>} />
         <Route  path='/logout' element={<Logout />} />
       </Routes>
     </Router>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,24 @@
+import { signInWithPopup } from 'firebase/auth'
 import React from 'react'
+import { auth, provider } from '../firebase'
+import { useNavigate } from 'react-router-dom'
 
-const Login = () => {
+const Login = ({ setIsAuth }) => {
+    const navigate = useNavigate()
+    const loginInWithGoogle = ()  =>   {
+        // googleでログイン
+        signInWithPopup(auth, provider).then((result) => {     //参照：https://firebase.google.com/docs/auth/web/google-signin
+            // ログインしたかどうかの状態変数の設定。ローカルストレージ
+            localStorage.setItem("isAuth", true)
+            setIsAuth(true)  //app.jsの値がtrueになる
+            navigate('/')  //login後にホームへ戻る
+        })
+    }
   return (
-    <div>Login</div>
+    <div>
+        <p>ログインして始める</p>
+        <button onClick={loginInWithGoogle}>Googleで始める</button>
+    </div>
   )
 }
 
