046f6c3ac6f7fe32f627d94f4088e420045a3457
Kazuki624 <audi_kazuki0624@yahoo.co.jp>
2022-11-15 17:05:15 +0900

    create component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from './components/Home';
+import CreatePost from './components/CreatePost';
+import Login from './components/Login';
+import Logout from './components/Logout';
+
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <Router>
+      <Routes>
+        <Route  path='/' element={<Home />} />
+        <Route  path='/createpost' element={<CreatePost />} />
+        <Route  path='/login' element={<Login />} />
+        <Route  path='/logout' element={<Logout />} />
+      </Routes>
+    </Router>
   );
 }
 
