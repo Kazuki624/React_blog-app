f5b09393b3a6a0303850eb1db579f3beb2440f05
Kazuki624 <audi_kazuki0624@yahoo.co.jp>
2022-11-16 23:22:50 +0900

    make home components and styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import "./Home.css"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='homePage'>
+        <div className='postContent'>
+            <div className='postHeader'>
+                <h1>タイトル</h1>
+            </div>
+            <div className='postTextContainer'>
+                hello world !!
+            </div>
+            <div className='nameAndDeleteButton'>
+                <h3>菅野一樹</h3>
+                <button>削除</button>
+            </div>
+        </div>
+    </div>
   )
 }
 
