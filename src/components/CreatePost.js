814dbb100baa37ef42531b196b71363c17a812ab
Kazuki624 <audi_kazuki0624@yahoo.co.jp>
2022-11-16 22:31:13 +0900

    get input post title and content

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./CreatePost.css";
 
 const CreatePost = () => {
+    //変数を格納するための状態変数をuseStateで宣言
+    const [title, setTitle] = useState();
+    const [postText, setPostText] = useState();
+
+    const createPost = () => {
+        console.log(title);
+        console.log(postText);
+    }
   return (
     <div className='createPostPage'>
         <div className='postContainer'>
             <h1>記事を投稿する</h1>
             <div className='inputPost'>
                 <div>タイトル</div>
-                <input type="text" placeholder="タイトルを入力してください"></input>
+                <input 
+                    type="text" 
+                    placeholder="タイトルを入力してください" 
+                    onChange={(e) => setTitle(e.target.value)}></input> {/*onChangeトリガーで入力されるたびにhooksのsetTitleが起動。イベントのターゲットのvalue(文字列)を取得する*/}
             </div>
             <div className='inputPost'>
                 <div>投稿内容</div>
-                <textarea type="text" placeholder="投稿内容を入力してください"></textarea>
+                <textarea 
+                    type="text" 
+                    placeholder="投稿内容を入力してください" 
+                    onChange={(e) => setPostText(e.target.value)}></textarea>
             </div>
-            <button className='postButton'>投稿する</button>
+            <button className='postButton' onClick={createPost}>投稿する</button>
         </div>
     </div>
   )
