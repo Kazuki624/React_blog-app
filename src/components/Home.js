63bd59f169c4d35f1baa9e8b40c480a09a6b40aa
Kazuki624 <audi_kazuki0624@yahoo.co.jp>
2022-11-16 23:45:27 +0900

    get all data from firestore

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {collection,getDocs} from "firebase/firestore"
+import {db} from "../firebase"
 import "./Home.css"
 
 const Home = () => {
+    const [postList, setPostList] = useState([])
+    // ページリロード時に一度だけブログを表示するので、useEffectを使用する
+    useEffect(() => {
+        const getPosts = async () => {
+            const data = await getDocs(collection(db, "posts"));
+            // console.log(data)
+            // console.log(data.docs)
+            // console.log(data.docs.map((doc) => ({doc})))
+            // console.log(data.docs.map((doc) => ({...doc.data(), id:doc.id}))) //data関数で中身のデータを取得してくる
+            setPostList((data.docs.map((doc) => ({...doc.data(), id:doc.id}))))
+        }
+        getPosts()
+    })
   return (
     <div className='homePage'>
-        <div className='postContent'>
-            <div className='postHeader'>
-                <h1>タイトル</h1>
-            </div>
-            <div className='postTextContainer'>
-                hello world !!
-            </div>
-            <div className='nameAndDeleteButton'>
-                <h3>菅野一樹</h3>
-                <button>削除</button>
-            </div>
-        </div>
+        {postList.map((post) => {
+            return (
+                <div className='postContent' key={post.id}>
+                    <div className='postHeader'>
+                        <h1>{post.title}</h1>
+                    </div>
+                    <div className='postTextContainer'>
+                            {post.postText}    
+                    </div>
+                    <div className='nameAndDeleteButton'>
+                        <h3>@{post.author.username}</h3>
+                        <button>削除</button>
+                    </div>
+                </div>
+            )
+        })}
     </div>
   )
 }
